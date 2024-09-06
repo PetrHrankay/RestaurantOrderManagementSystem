d7823a7ff6da58c49a20e4a010550338e6a72f00
P3ANTMC <pean.tmc@seznam.cz>
2024-09-06 22:54:30 +0200

    averageOrderProcessingTimeInMinutes method added.
    This method is used to calculate the average time required to complete an order.

--- src/cz/engeto/ja2024/RestaurantManager.java
+++ src/cz/engeto/ja2024/RestaurantManager.java
@@ -1,6 +1,8 @@
 package cz.engeto.ja2024;
 
-import java.util.ArrayList;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 
@@ -21,4 +23,30 @@ public class RestaurantManager {
         orders.sort(Comparator.comparing(Order::getOrderedTime));
         return orders;
     }
+
+    public static String averageOrderProcessingTimeInMinutes() {
+        List<Order> orders = Order.getAllOrdersFromReceivedOrdersList();
+        long totalProcessingTime = 0;
+        int completedOrdersCount = 0;
+
+        for (Order order : orders) {
+            LocalDateTime start = order.getOrderedTime();
+            LocalDateTime end = order.getFulfilmentTime();
+            if (end == null) {
+                continue;
+            }
+            long processingTime = Duration.between(start, end).toMinutes();
+            totalProcessingTime += processingTime;
+            completedOrdersCount++;
+        }
+        if (completedOrdersCount == 0) {
+            return "No completed orders available";
+        }
+        long averageTime = totalProcessingTime / completedOrdersCount;
+        String result = "The average order processing time is: " + averageTime + " minutes.";
+        System.out.println(result);
+        return result;
+        }
 }
+
+
