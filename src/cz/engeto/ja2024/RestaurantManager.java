6f5fef1617c1a2a4aa0f854aa6d1153b76ad93e6
P3ANTMC <pean.tmc@seznam.cz>
2024-09-06 21:05:47 +0200

    getOrdersSortedByCreationTime method added.
    This method is used to sort orders by the date and time they were created.

--- src/cz/engeto/ja2024/RestaurantManager.java
+++ src/cz/engeto/ja2024/RestaurantManager.java
@@ -1,6 +1,7 @@
 package cz.engeto.ja2024;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class RestaurantManager {
@@ -13,5 +14,11 @@ public class RestaurantManager {
             }
         }
         return pendingOrders;
+    }
+
+    public static List<Order> getOrdersSortedByCreationTime() {
+        List<Order> orders = Order.getAllOrdersFromReceivedOrdersList();
+        orders.sort(Comparator.comparing(Order::getOrderedTime));
+        return orders;
     }
 }
